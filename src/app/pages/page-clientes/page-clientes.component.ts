81615bdb397ba1754ade9c004de63647e8cb5c56
Lider <lial2010@hotmail.es>
2022-08-01 16:21:42 -0500

    avances pantalla clientes

--- src/app/pages/page-clientes/page-clientes.component.ts
+++ src/app/pages/page-clientes/page-clientes.component.ts
@@ -1,5 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Cliente{
+  ci: String,
+  nombre: String,
+  tipo: String,
+  email: String,
+  telefono: String,
+  nacionalidad: String
+}
+
+const clientesData : Cliente[] = [
+  {ci: '1316530177', nombre: 'Lider Ignacio Andrade Cevallos', tipo: 'Recurrente', email:'lial2010@hotmail.es', telefono: '0969841775', nacionalidad: 'Ecuador'},
+  {ci: '3434235674', nombre: 'Juan Fernandez', tipo: 'Normal', email:'ljuanfer43@hotmail.com', telefono: '0969841775', nacionalidad: 'Ecuaor'},
+  {ci: '3434235674', nombre: 'Juan Fernandez', tipo: 'Normal', email:'ljuanfer43@hotmail.com', telefono: '0969841775', nacionalidad: 'Ecuaor'},
+  {ci: '3434235674', nombre: 'Juan Fernandez', tipo: 'Normal', email:'ljuanfer43@hotmail.com', telefono: '0969841775', nacionalidad: 'Ecuaor'},
+  {ci: '3434235674', nombre: 'Juan Fernandez', tipo: 'Normal', email:'ljuanfer43@hotmail.com', telefono: '0969841775', nacionalidad: 'Ecuaor'},
+  {ci: '3434235674', nombre: 'Juan Fernandez', tipo: 'Normal', email:'ljuanfer43@hotmail.com', telefono: '0969841775', nacionalidad: 'Ecuaor'},
+  {ci: '3434235674', nombre: 'Juan Fernandez', tipo: 'Normal', email:'ljuanfer43@hotmail.com', telefono: '0969841775', nacionalidad: 'Ecuaor'},
+  {ci: '3434235674', nombre: 'Juan Fernandez', tipo: 'Normal', email:'ljuanfer43@hotmail.com', telefono: '0969841775', nacionalidad: 'Ecuaor'},
+  {ci: '3434235674', nombre: 'Juan Fernandez', tipo: 'Normal', email:'ljuanfer43@hotmail.com', telefono: '0969841775', nacionalidad: 'Ecuaor'},
+  {ci: '3434235674', nombre: 'Juan Fernandez', tipo: 'Normal', email:'ljuanfer43@hotmail.com', telefono: '0969841775', nacionalidad: 'Ecuaor'},
+  {ci: '3434235674', nombre: 'Juan Fernandez', tipo: 'Normal', email:'ljuanfer43@hotmail.com', telefono: '0969841775', nacionalidad: 'Ecuaor'}
+];
+
 @Component({
   selector: 'app-page-clientes',
   templateUrl: './page-clientes.component.html',
@@ -7,7 +30,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageClientesComponent implements OnInit {
 
-  displayedColumns: string[] = ['Ci/Ruc', 'Nombre', 'Tipo', 'Email', 'Telefono', 'Nacionalidad'];
+  displayedColumns: string[] = ['ci', 'nombre', 'email', 'tipo', 'telefono', 'nacionalidad', 'actions'];
+  datasource = clientesData;
 
   constructor() { }
 
