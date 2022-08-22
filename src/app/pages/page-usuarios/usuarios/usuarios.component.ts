e570d93855fa2682d84d6bafb0b96a78beb93510
Lider <lial2010@hotmail.es>
2022-08-22 15:38:39 -0500

    avances

--- src/app/pages/page-usuarios/usuarios/usuarios.component.ts
+++ src/app/pages/page-usuarios/usuarios/usuarios.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
+import { TokenValidate } from 'src/app/interfaces/IWebData';
 import { Usuario } from 'src/app/interfaces/Usuario';
+import { LocalService } from 'src/app/services/local.service';
 
 @Component({
   selector: 'app-usuarios',
@@ -14,15 +17,47 @@ export class UsuariosComponent implements OnInit {
   displayedColumns: string[] = ['nombre', 'telefono', 'email', 'username'];
   datasource = new MatTableDataSource<Usuario>();
 
+  showPagination = false;
+  showSinDatos = false;
+  isLoading = false;
+  listaUsuarios: Usuario[] = [];
+
+  idEmpresa: number = 0;
+  rucEmpresa: string = '';
+  //dataUser
+  dataUser: any;
+  tokenValidate!: TokenValidate;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild('containerTable', {read: ElementRef}) tableInput!: ElementRef
+
   constructor(private formBuilder: FormBuilder,
+              private localService: LocalService,
               private router: Router) { 
   }
 
   ngOnInit(): void {
+
+    // GET INITIAL DATA 
+    const localServiceResponseToken = this.localService.storageGetJsonValue('DATA_TOK');
+    const localServiceResponseUsr = this.localService.storageGetJsonValue('DATA_USER');
+
+    this.dataUser = localServiceResponseToken;
+    const { token, expire } = this.dataUser;
+    this.tokenValidate = { token, expire};
+
+    this.idEmpresa = localServiceResponseUsr._bussId;
+    this.rucEmpresa = localServiceResponseUsr._ruc;
+
+
   }
 
 
   nuevoUsuario(){
     this.router.navigate(['/usuarios/nuevo']);
+  }
+
+  private getListUsuariosRefresh(): void {
+    this.isLoading = !this.isLoading
   }
 }
