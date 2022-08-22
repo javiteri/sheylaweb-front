e35d36a8706b80487e6e7a7cf1f74e5281d17556
Lider <lial2010@hotmail.es>
2022-08-22 15:38:48 -0500

    cambios

--- src/app/interfaces/Usuario.ts
+++ src/app/interfaces/Usuario.ts
@@ -1,6 +1,12 @@
 export interface Usuario {
+    id: number,
+    empresaId: number
     nombre: string,
     telefono: string,
+    direccion: string,
     email: string,
-    username: string
+    fechaNacimiento: string,
+    username: string,
+    password: string,
+    permisoEscritura: number
 }
