2267180e0dbcf3805f60c904f4220480da06a575
Lider <lial2010@hotmail.es>
2022-12-21 13:07:58 -0500

    avances

--- src/app/pages/page-clientes/lista-proformas/lista-proformas.component.ts
+++ src/app/pages/page-clientes/lista-proformas/lista-proformas.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { TokenValidate } from 'src/app/interfaces/IWebData';
+import { ItemListaProforma } from '../models/ItemListaProforma';
 
 @Component({
   selector: 'app-lista-proformas',
@@ -7,6 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaProformasComponent implements OnInit {
 
+  displayedColumns: string[] = ['fecha hora', 'documento', 'numero', 'total', 'usuario', 'cliente', 'identificacion', 'forma pago','actions'];
+  datasource = new MatTableDataSource<ItemListaProforma>();
+
+  idEmpresa: number = 0;
+  rucEmpresa: string = '';
+  nombreBd: string = '';
+  //dataUser
+  dataUser: any;
+  tokenValidate!: TokenValidate;
+
+  showPagination = false;
+  showSinDatos = false;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  dateInicioFilter = new Date();
+  dateFinFilter = new Date();
+
+  noDocmento = "";
+  nombreCiRuc = "";
+  
   constructor() { }
 
   ngOnInit(): void {
